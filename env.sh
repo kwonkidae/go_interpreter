665b6045b5d373548ec22c3ec47708096ac22c04
kwon <kkdosk@naver.com>
2019-06-20 11:13:44 +0900

    ast

--- env.sh
+++ env.sh
@@ -1,3 +1,8 @@
 export GOPATH=$GOPATH:$PWD
-go build main.go && ./main
-# go test ./monkey/lexer
+# go build main.go && ./main
+# for monkey test
+# cd src/monkey
+# go test ./lexer
+
+ cd src/monkey
+ go test ./parser
